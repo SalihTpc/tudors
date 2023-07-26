996aee36a613ee3fa21f333e91ffd79dcc15e09c
SalihTpc <salihtopcu@hotmail..com.tr>
2023-07-26 15:33:40 +0300

    tailwind config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        inter: ["Inter"],
+      },
       colors: {
         tudorsGray: "#F0F0F0",
+        tudorsBlue: "#2148C0",
       },
     },
   },
