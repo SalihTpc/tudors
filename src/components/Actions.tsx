2f6a7b47cc32d502e5b4ff7382a42e158ccef967
SalihTpc <salihtopcu@hotmail..com.tr>
2023-07-26 15:35:35 +0300

    selected and url added.

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,17 +1,26 @@
 import Action from "./Action";
 import playSvg from "../assets/icons/play.svg";
 import stopSvg from "../assets/icons/stop.svg";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Actions = () => {
+  const [selected, setSelected] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    setSelected(location.pathname.slice(1));
+  }, [location.pathname]);
   return (
-    <div className="flex items-center justify-center my-10 [&>*]:mr-8">
+    <div className="flex items-center justify-center my-10 [&>*]:mr-8 font-inter">
       <Action
         title={"Sipariş Aktarımı"}
         icon={playSvg}
         action={"Running ( 04:14 )"}
         process1={"18"}
         process2={"2"}
-        selected={false}
+        selected={selected.endsWith("ordertransfer")}
+        toUrl="/dashboard/ordertransfer"
       />
       <Action
         title={"Kargo Entegrasyonu"}
@@ -19,7 +28,8 @@ const Actions = () => {
         action={"Stopped"}
         process1={"84"}
         process2={"0"}
-        selected={false}
+        selected={selected.endsWith("orderentegration")}
+        toUrl="/dashboard/orderentegration"
       />
       <Action
         title={"Stok - Fiyat Eşitleme"}
@@ -27,7 +37,8 @@ const Actions = () => {
         action={"Stopped"}
         process1={"145845"}
         process2={"0"}
-        selected={false}
+        selected={selected.endsWith("stockequal")}
+        toUrl="/dashboard/stockequal"
       />
     </div>
   );
