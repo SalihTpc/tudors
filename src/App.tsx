2f40e7ad9f8fa1cee04d9e12fa7dd1030ea1512e
SalihTpc <salihtopcu@hotmail..com.tr>
2023-07-26 15:34:27 +0300

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import Actions from "./components/Actions";
-import Conditions from "./components/Conditions";
-import Dashbord from "./components/Dashbord";
-import Navbar from "./components/Navbar";
-import Table from "./components/Table";
+import { Suspense, lazy } from "react";
+import { Spin } from "antd";
+import { Route, Routes } from "react-router-dom";
+
+const Main = lazy(() => import("./views/Home"));
+const Login = lazy(() => import("./views/Login"));
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Dashbord />
-      <Actions />
-      <Conditions />
-      <Table />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-w-screen min-h-screen">
+            <Spin tip="Loading..." size="large">
+              <div className="p-12 bg-gray-300 rounded-lg" />
+            </Spin>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Main />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
