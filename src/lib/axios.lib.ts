5e698c6749bf70765ff9fb337b648d28d75475db
SalihTpc <salihtopcu@hotmail..com.tr>
2023-07-26 15:36:33 +0300

    axios

---
+++ src/lib/axios.lib.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosRequestHeaders } from "axios";
+import { baseURL } from "../configuration/config";
+import { logout } from "../store/features/auth/auth.slice";
+import { store } from "../store/store";
+import Cookies from "js-cookie";
+
+const headers: any = axios.defaults.headers;
+headers.common["Content-Type"] = "*/*";
+
+const instance = axios.create({
+  baseURL,
+  headers: headers,
+});
+
+instance.interceptors.request.use((config: any) => {
+  config.headers.Authorization = `Bearer ${Cookies.get("token")}`;
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response: any) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status == 401) {
+      store.dispatch(logout());
+    }
+    if (error.response && error.response.data) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error.message);
+  }
+);
+
+export { instance as axios };
