73eee371b829bffcaa9eff6d8db48ad78608cb50
SalihTpc <salihtopcu@hotmail..com.tr>
2023-09-10 14:20:57 +0300

    redux closed

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  // <Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </Provider>
 );
