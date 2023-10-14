ec4d2c6651a20eaf3c9bdb6193bc7bab00679d10
SalihTpc <salihtopcu@hotmail.com.tr>
2023-10-15 00:43:25 +0300

    redux online

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { store } from "./store/store.ts";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
