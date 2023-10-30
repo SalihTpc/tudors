f31ad67bc8abe03b071604b6974d03ab0d5917c2
SalihTpc <salihtopcu@hotmail.com.tr>
2023-10-30 15:51:08 +0300

    settings endpoints added.

--- src/api/settings.api.ts
+++ src/api/settings.api.ts
@@ -2,8 +2,67 @@ import { baseURL } from "../lib/config";
 import { axios as myAxios } from "../lib/axios.lib";
 
 class SettingsApi {
-  async generalParams() {
+  async getGeneralParams() {
     const response = await myAxios.get(baseURL + "general/generalparameters");
+    return response.data;
+  }
+
+  async updateGeneralParams(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "general/updategeneralgarameters",
+      params
+    );
+    return response.data;
+  }
+
+  async getPaymentParams() {
+    const response = await myAxios.get(baseURL + "general/paymentparameters");
+    return response.data;
+  }
+
+  async updatePaymentParams(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "general/updatepaymentparameter",
+      params
+    );
+    return response.data;
+  }
+
+  async getSpecialParams() {
+    const response = await myAxios.get(baseURL + "general/specialparameters");
+    return response.data;
+  }
+
+  async updateSpecialParams(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "general/updatespecialparameter",
+      params
+    );
+    return response.data;
+  }
+
+  async getSmtpParams() {
+    const response = await myAxios.get(baseURL + "general/smtpparameters");
+    return response.data;
+  }
+
+  async updateSmtpParams(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "general/updatesmtpparameter",
+      params
+    );
+    return response.data;
+  }
+
+  async getOrderSources() {
+    const response = await myAxios.post(baseURL + "general/getsipariskaynagi");
+    return response.data;
+  }
+
+  async getPaymentStatus() {
+    const response = await myAxios.post(
+      baseURL + "general/getticimacodemetipi"
+    );
     return response.data;
   }
 }
