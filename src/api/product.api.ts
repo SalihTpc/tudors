c7e047fe8d6bde2ee0c2d00ee299364fb362e435
SalihTpc <salihtopcu@hotmail.com.tr>
2023-11-20 18:41:55 +0300

    new endpoints added.

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -13,6 +13,37 @@ class ProductApi {
     const response = await myAxios.post(baseURL + "product/stopproductjob");
     return response.data;
   }
+  async getStockLastExecuteTime() {
+    const response = await myAxios.post(
+      baseURL + "product/getstoklastexecutetime",
+      {}
+    );
+    return response.data;
+  }
+
+  async getStockServiceReport(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "product/stokservicestate",
+      params
+    );
+    return response.data;
+  }
+
+  async getStockServiceLog(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "product/stokservicelog",
+      params
+    );
+    return response.data;
+  }
+
+  async startJobNov(params: {}) {
+    const response = await myAxios.post(
+      baseURL + "product/startproductjobnow",
+      params
+    );
+    return response.data;
+  }
 }
 
 export default new ProductApi();
