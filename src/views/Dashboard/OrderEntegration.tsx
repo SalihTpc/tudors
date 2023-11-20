29edc57f660eb712b8733cfa093284aeceedc01d
SalihTpc <salihtopcu@hotmail.com.tr>
2023-11-20 18:54:31 +0300

    Actions replaced.

--- src/views/Dashboard/OrderEntegration.tsx
+++ src/views/Dashboard/OrderEntegration.tsx
@@ -1,6 +1,6 @@
 import EntConditions from "../../components/EntConditions";
 import RecordedSettings from "../../components/RecordedSettings";
-import Table from "../../components/List";
+import Table from "../../components/logs/Order/List";
 
 const OrderEntegration = () => {
   return (
--- src/views/Logs/Index.tsx
+++ src/views/Logs/Index.tsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from "react";
 import { Spin } from "antd";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Actions from "../../components/Actions";
 
 const Logs = lazy(() => import("./Logs"));
 
@@ -16,6 +17,7 @@ const LogsIndex = () => {
       }
     >
       <div className="my-4 mx-48 font-inter">
+        <Actions />
         <Routes>
           <Route path="" element={<Logs />} />
           <Route path="*" element={<Navigate to="/dashboard" />} />
